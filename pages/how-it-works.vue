aacde3b7f1ba55681401de3d2625d3f0e909d4c2
SharyZ <onshariiit@gmail.com>
2020-10-17 22:06:42 +0500

    feat: add how-it-works page

--- pages/how-it-works.vue
+++ pages/how-it-works.vue
@@ -1,5 +1,103 @@
 <template>
   <div>
-    <h1>How it works</h1>
+    <h1>Bu qanday ishlaydi?</h1>
+    <div class="flex items-center justify-center my-24">
+      <div class="w-1/3 px-5">
+        <img
+          :src="require('~/assets/how-it-works/undraw_Process.svg')"
+          alt=""
+          class="w-full"
+        />
+      </div>
+      <div class="w-2/3 px-5">
+        <h3 class="leading-tight mb-5">Avtorizatsiya va murojaatni yaratish</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-row-reverse items-center justify-center my-24">
+      <div class="w-1/3 px-5">
+        <img
+          :src="require('~/assets/how-it-works/undraw_file_searching.svg')"
+          alt=""
+          class="w-full"
+        />
+      </div>
+      <div class="w-2/3 px-5">
+        <h3 class="leading-tight mb-5">
+          Ekspertiza va elektron murojaatni joylash
+        </h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+      </div>
+    </div>
+    <div-section class="flex items-center justify-center my-24">
+      <div class="w-1/3 px-5">
+        <img
+          :src="require('~/assets/how-it-works/undraw_like_dislike.svg')"
+          alt=""
+          class="w-full"
+        />
+      </div>
+      <div class="w-2/3 px-5">
+        <h3 class="leading-tight mb-5">Murojaatga ovoz berish</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+      </div>
+    </div-section>
+    <div class="flex flex-row-reverse items-center justify-center my-24">
+      <div class="w-1/3 px-5">
+        <img
+          :src="require('~/assets/how-it-works/undraw_Done_checking.svg')"
+          alt=""
+          class="w-full"
+        />
+      </div>
+      <div class="w-2/3 px-5">
+        <h3 class="leading-tight mb-5">Murojaatni ko'rib chiqish</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          consequatur eum, nostrum deserunt consequuntur quam minus blanditiis
+          excepturi ut alias hic facere, mollitia voluptatibus optio amet ea
+          numquam quas distinctio!
+        </p>
+      </div>
+    </div>
   </div>
 </template>
